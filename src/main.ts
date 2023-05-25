7e0fdd020d2ba824735df080eb98d76d46bfedd7
Sebastian Letelier <sebalete10@gmail.com>
2023-05-25 14:59:52 -0400

    Api pokemon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Asigna el /api de forma global
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
