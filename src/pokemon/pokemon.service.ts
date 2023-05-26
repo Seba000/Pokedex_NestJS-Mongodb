af4cc0f9afb447b58f0905ec190e7eed630d8954
Sebastian Letelier <sebalete10@gmail.com>
2023-05-26 11:20:30 -0400

    Validacion de post

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,12 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
+
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    //validaciones
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
-    return createPokemonDto;
+    try {
+      //db
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if(error.code === 11000){
+        throw new BadRequestException(`Pokemon already exists ${JSON.stringify(error.keyValue)}`);
+      }
+      console.log(error);
+      throw new InternalServerErrorException(`Can't create pokemon - Check server logs`)
+    }
+
   }
 
   findAll() {
